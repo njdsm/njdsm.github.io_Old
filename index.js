0e919348fdab3644a74fa93fe066f353e90cbc90
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-04-07 11:35:10 -0500

    integrated responsive menu bar.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-"use strict"
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+})
 
