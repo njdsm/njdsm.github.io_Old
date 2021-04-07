1951f3a27714fbedea304594755e2295efbfb2b2
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-04-07 13:06:43 -0500

    Changed color scheme

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
+            $('.bar').addClass("sticky");
         }
         else{
             $('.navbar').removeClass("sticky");
+            $('.bar').removeClass("sticky");
         }
     })
 })
